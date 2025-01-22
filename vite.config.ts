a30a2e911e38dbb25e168b6cce167bb09e67a914
Tevin Aduma <tevin74@live.com>
2025-01-22 19:49:04 +0300

    task: config vite to listen on loopback address as well

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import "dotenv/config";
 export default defineConfig({
 	plugins: [sveltekit()],
 	preview: {
-		host: process.env.VITE_PROD_HOST ?? '127.0.0.1',
+		host: '0.0.0.0',
 		port: parseInt(process.env.VITE_PROD_PORT ?? "5173")
 	},
 	server: {
-		host: process.env.VITE_DEV_HOST ?? '127.0.0.1',
+		host: '0.0.0.0',
 		port: parseInt(process.env.VITE_DEV_PORT ?? "5173")
 	}
 });
